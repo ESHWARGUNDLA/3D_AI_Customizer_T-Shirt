67ef6c81144032a2ff8f923d6a6244eeeb64d36d
ESHWARGUNDLA <84794875+ESHWARGUNDLA@users.noreply.github.com>
2024-04-12 17:31:13 +0000

    deploying

--- server/routes/dalle_routes.js
+++ server/routes/dalle_routes.js
@@ -1,24 +1,39 @@
 import express from "express";
 import * as dotenv from "dotenv";
-import cors from "cors";
+import OpenAI from "openai";
 
-import dalleRoutes from "./routes/dalle_routes.js";
 dotenv.config();
 
-const app = express();
+const router = express.Router();
 
-// Use cors middleware
-app.use(cors());
-
-// Parse JSON requests
-app.use(express.json({ limit: "50mb" }));
-
-app.use("/api/v1/dalle", dalleRoutes);
-
-// Define a route
-app.get("/", (req, res) => {
-  res.status(200).json({ message: "hello from DALL E" });
+const config = new OpenAI({
+  apiKey: process.env.OPENAI_API_KEY,
 });
 
-// Start the server
-app.listen(8080, () => console.log("Server has started on port 8080"));
+const openai = new OpenAI(config);
+
+router.route("/").get((req, res) => {
+  res.status(200).json({ message: "Hello from DALL.E ROUTES" });
+});
+
+router.route("/").post(async (req, res) => {
+  try {
+    const { prompt } = req.body;
+
+    const response = await openai.images.generate({
+      prompt,
+      n: 1,
+      size: "1024x1024",
+      response_format: "b64_json",
+    });
+
+    const image = response.data.data[0].b64_json;
+
+    res.status(200).json({ photo: image });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ message: "Something went wrong" });
+  }
+});
+
+export default router;
